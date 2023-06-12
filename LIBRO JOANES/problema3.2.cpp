67fd7c2a19f6349f10b83c2c87a738ac1dbf23df
RoqueAlicia <135660050+RoqueAlicia@users.noreply.github.com>
2023-06-12 11:01:27 -0500

    Update problema3.2.cpp
    respondido

--- LIBRO JOANES/problema3.2.cpp
+++ LIBRO JOANES/problema3.2.cpp
@@ -11,6 +11,14 @@ int main(int argc , char * argv[]) // QUE SIGNIFICAN ESOS PARAMETROS
     
     return 0;
 }
+#include <cstdlib> // PARA QUE UTILIZAMOS ESTA LIBRERIA
+#include<cstdlb> para indicar al copilador que inserte el codigo fuente en el registro programa y lo compile con el programa principal
+<cstdlib> se utiliza para convertir un entero a cadena de caracteres
 
-
+int main(int argc , char * argv[]) // QUE SIGNIFICAN ESOS PARAMETROS
+intes el tipo de dato de retorno de la función
+main():Indica que la función devuelve
+int argc: es el nurmero de argumetros que se pasan a su progrma desde la linea de comandos.
+    char:se usa para almacenar el valor entero 
+    argv: es la direccion de una tabla
 
